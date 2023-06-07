0071e74bfa9d8c6810748cb52654a31a76af048e
Darash Mishra <darash.mishra@gmail.com>
2023-06-07 09:28:15 +0530

    Contact US

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/public'));
 
 app.get('/', (req, res) => {
-    res.render('contact');
+    res.render('contact')
 })
 
 app.post('/', (req, res) => {
